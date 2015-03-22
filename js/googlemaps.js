b548fed34a168608d20e8e9d554f19024e830a1b
Arielos <liorar53@gmail.com>
2015-03-22 09:10:40 +0200

    Image upload update
    Added geo location feature for the map

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,40 +1,47 @@
 $(function() {
     google.maps.event.addDomListener(window, 'load', initialize);
 
-/*
     $.getJSON(
         'functions/getSpot.php'
     ).done(function(data) {
         $.each(data, function(i, item) {
-            alert("i: " + i, "item: " + item);
+            console.log("i: " + i, "item: " + item);
             return true;
         });
     });
-    */
-    //var coords = new google.maps.LatLng(32.09374408993661, 35.13043239712715);
-    //var m = placeMarker(coords, map);
+    console.log("google map script ready done!");
 });
 
+var marker = null;
+var map = null;
+
 function initialize() {
+
 	var mapOptions = {
 		center: { lat:31.4117256, lng: 35.0818155},
 		zoom: 7
     };
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	google.maps.event.addListener(map, 'click', function(e) {
 
-		//if (marker != null) {
-		//	marker.setMap(null);
-		//}
+		if (marker != null) {
+			marker.setMap(null);
+		}
 
-		var marker = placeMarker(e.latLng, map);
+		marker = placeMarker(e.latLng, map);
         google.maps.event.addListener(marker, 'click', function() {
             updateCoordsLabel(marker);
         });
 
         updateCoordsLabel(marker);
     });
+
+    if(window.navigator.geolocation) {
+        window.navigator.geolocation.getCurrentPosition(myfunc, errfunc);
+    } else {
+        console.log("geolocation is not supported");
+    }
 }
 
 function placeMarker(position, map) {
@@ -50,4 +57,16 @@ function placeMarker(position, map) {
 function updateCoordsLabel(m) {
     $('#gps-coordinates-label').empty();
     $('#gps-coordinates-label').append('GPS Coordinates: ' + m.position.lat() + ', ' + m.position.lng());
+}
+
+function myfunc(ob) {
+    var myLatlng = new google.maps.LatLng(ob.coords.latitude, ob.coords.longitude);
+
+    marker = placeMarker(myLatlng, map);
+    updateCoordsLabel(marker);
+
+    console.log("latitude=" + ob.coords.latitude +" longitude="+ ob.coords.longitude);
+}
+function errfunc(ob) {
+    console.log(ob.message);
 }
